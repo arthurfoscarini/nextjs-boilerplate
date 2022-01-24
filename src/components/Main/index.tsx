aa7bb3e29a5b34f99bce558416ef143aaa728125
Arthur Foscarini <arthur.foscarini@gmail.com>
2022-01-23 21:54:53 -0300

    Add remaining configs

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,18 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/icon-192.png"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>Learning Next</S.Title>
+    <S.Description>Description</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Picture of a developer in front of a screen with code on it."
+    />
+  </S.Wrapper>
+)
+
+export default Main
